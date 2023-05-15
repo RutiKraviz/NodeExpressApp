e9d67b7fa6e6503f0289d62b1cfb3047665073c9
RutiKraviz <r0548494906@gmail.com>
2023-05-15 21:37:46 +0300

    add node project

---
+++ index.js
@@ -0,0 +1,15 @@
+// import express from 'express';
+const express = require('express');
+const app = express()
+const port = process.env.PORT || 3000
+const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
+
+sdk.auth('rnd_5yHz2HcFny5b2iaRk92TMhEspKm1');
+
+app.get("/", (req, res) => {
+  sdk.getServices({ limit: '20' })
+  .then(({ data }) => console.log(data))
+  .catch(err => console.error(err));
+})
+
+app.listen(port, () => { console.log(`app listening on http://localhost:${port}`) })
