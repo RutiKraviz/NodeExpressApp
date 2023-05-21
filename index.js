de6cfcece416001c7ebdda28038da0c73477b743
RutiKraviz <r0548494906@gmail.com>
2023-05-21 19:51:41 +0300

    add send to get function

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const app = express()
-const port = process.env.PORT || 3000
+const port = 3000
+// || process.env.PORT
 const sdk = require('api')('@render-api/v1.0#dnrc1ulf088q9j');
 
 sdk.auth('rnd_5yHz2HcFny5b2iaRk92TMhEspKm1');
 
 app.get("/", (req, res) => {
   sdk.getServices({ limit: '20' })
-  .then(({ data }) => console.log(data))
-  .catch(err => console.error(err));
+    .then(({ data }) => {
+      res.send(data);
+      console.log(data)
+    })
+    .catch(err => console.error(err));
 })
 
 app.listen(port, () => { console.log(`app listening on http://localhost:${port}`) })
